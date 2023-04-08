ac95f054c1a182316f56006160b2750771c69c6b
kazune-br <kazune.miyagi92@gmail.com>
2023-04-08 17:49:55 +0900

    feat: add deck.gl and LineLayer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import {useState} from 'react'
+import DeckGL from '@deck.gl/react/typed';
+import {LineLayer} from '@deck.gl/layers/typed';
 import './App.css'
+
+const INITIAL_VIEW_STATE = {
+    longitude: -122.41669,
+    latitude: 37.7853,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0,
+};
+
+const data = [
+    {
+        sourcePosition: [-122.41669, 37.7853],
+        targetPosition: [-122.41669, 37.781]
+    }
+];
+
 
 function App() {
     const [count, setCount] = useState(0)
     const [date, setDate] = useState("2023-03-23");
+    const layers = [
+        new LineLayer({id: 'line-layer', data})
+    ];
 
     return (
         <div className="App">
@@ -26,6 +47,11 @@ function App() {
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
+            <DeckGL
+                initialViewState={INITIAL_VIEW_STATE}
+                controller={true}
+                layers={layers}
+            />
         </div>
     )
 }
